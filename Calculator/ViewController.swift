45ed8d09c0b69f13e99ff19c731a8f71484a637f
Jeff Patterson <jeff_patterson@outlook.com>
2025-03-13 06:18:05 -0500

    moving on to lesson 307

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,20 +12,61 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var displayLabel: UILabel!
     
+    /// This variable is used to check if the user has finished typing the number
+    private var isFinsihedTypingNumber: Bool = true
     
-    
+    /// What should happen when a non-number button is pressed
     @IBAction func calcButtonPressed(_ sender: UIButton) {
+        isFinsihedTypingNumber = true
         
-        //What should happen when a non-number button is pressed
-    
+        /// This will crash with a usable error message if the text cannot be converted to a Double
+        guard let number = Double(displayLabel.text!) else {
+            fatalError("Cannot convert display label text to a Double")
+        }
+        
+        if let calcMethod = sender.currentTitle{
+            
+            switch calcMethod {
+                case "AC":
+                    displayLabel.text = "0"
+                    
+                case "+/-":
+                    displayLabel.text = String(number * -1)
+                    
+                case "%":
+                    displayLabel.text = String(number / 100)
+                    
+                default:
+                    break
+            }
+        }
+            
     }
-
     
+    /// What should happen when a number is entered into the keypad
     @IBAction func numButtonPressed(_ sender: UIButton) {
-        
-        //What should happen when a number is entered into the keypad
-    
+        if let numValue = sender.currentTitle {
+            if isFinsihedTypingNumber {
+                displayLabel.text = numValue
+                isFinsihedTypingNumber = false
+            } else {
+                
+                if numValue == "." {
+                    guard !displayLabel.text!.contains(".") else {
+                        fatalError(
+                            "Cannot have multiple decimal points in a number"
+                        )
+                    }
+                    
+                    let isInt floor(Double(displayLabel.text!)) == Double(displayLabel.text!)
+                    
+                }
+                displayLabel.text! += numValue
+            }
+        }
     }
-
+    
+    
+    
 }
 
