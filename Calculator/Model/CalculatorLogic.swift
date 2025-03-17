797321264cf534501b5b07e4dfcd89ed922267c0
Jeff Patterson <jeff_patterson@outlook.com>
2025-03-17 06:16:47 -0500

    Exercise 309  completed

--- Calculator/Model/CalculatorLogic.swift
+++ Calculator/Model/CalculatorLogic.swift
@@ -8,3 +8,21 @@
 
 import Foundation
 
+struct CalculatorLogic {
+    var number: Double
+    
+    init(number: Double) {
+        self.number = number
+    }
+        
+    func calculate(symbol: String) -> Double? {
+        if symbol == "AC" {
+            return 0
+        } else if symbol == "+/-"{
+            return number * -1
+        } else if symbol == "%" {
+                return number * 0.01
+        }
+        return nil
+    }
+}
