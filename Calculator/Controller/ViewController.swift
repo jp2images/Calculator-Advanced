8418a863ca71ba5b148d5a76a4339a33061e04aa
Jeff Patterson <jeff_patterson@outlook.com>
2025-03-19 06:18:31 -0500

    Beginning lesson 314

--- Calculator/Controller/ViewController.swift
+++ Calculator/Controller/ViewController.swift
@@ -29,11 +29,13 @@ class ViewController: UIViewController {
         }
     }
     
+    private var calculator = CalculatorLogic()
+    
     /// What should happen when a non-number button is pressed
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         isFinsihedTypingNumber = true
+        calculator.setNumber(displayValue)
         if let calcMethod = sender.currentTitle {
-            let calculator = CalculatorLogic(number: displayValue)
             guard let result = calculator.calculate(symbol: calcMethod) else {
                 fatalError("The result of the calculation is nil")
             }
--- Calculator/Model/CalculatorLogic.swift
+++ Calculator/Model/CalculatorLogic.swift
@@ -9,19 +9,31 @@
 import Foundation
 
 struct CalculatorLogic {
-    var number: Double
     
-    init(number: Double) {
+    private var number: Double?
+    
+    /// Make this tuple an optional by appending the ? to the end of the entire type.
+    private var intermediateCalculation: (number1: Double, calcMethod: String)?
+   
+    mutating func setNumber(_ number: Double) {
         self.number = number
     }
         
-    func calculate(symbol: String) -> Double? {
-        if symbol == "AC" {
-            return 0
-        } else if symbol == "+/-"{
-            return number * -1
-        } else if symbol == "%" {
-                return number * 0.01
+    mutating func calculate(symbol: String) -> Double? {
+        if let n = number {
+            if symbol == "AC" {
+                return 0
+            } else if symbol == "+/-"{
+                return n * -1
+            } else if symbol == "%" {
+                return n * 0.01
+            } else if symbol == "+" {
+                intermediateCalculation = (number1: n, calcMethod: symbol)
+            
+                
+            } else if symbol == "=" {
+                
+            }
         }
         return nil
     }
